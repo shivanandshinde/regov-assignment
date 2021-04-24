ec4c34418d3ba948e00d6f1e45d7fd027ec101a2
shivanand <shiva.chhamu@gmail.com>
2021-04-24 14:00:25 +0800

    add modules

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "rds" {
+  source = "./modules/rds"
+}
+
+module "s3" {
+  source = "./modules/s3"
+}
+
+module "dynamodb" {
+  source = "./modules/dynamodb"
+}
+
+module "lambda" {
+  source = "./modules/lambda"
+}
+
+module "cloudwatch" {
+  source = "./modules/cloudwatch"
+}
